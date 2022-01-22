869f2d7364bbc10439e3c0ed2095387608ddc10a
cooldrew4567 <85810206+cooldrew4567@users.noreply.github.com>
2022-01-22 01:10:39 -0800

    Update HiLo.py

--- HiLo.py
+++ HiLo.py
@@ -21,9 +21,9 @@ class Deal:
     def compare(self,guess):
         prevScore = self.cards[len(self.cards)-2]
         curScore = self.cards[len(self.cards)-1]
-        if curScore >= prevScore and guess == 'h':
+        if curScore >= prevScore and guess == 'H':
             self.score = self.score + 100
-        elif curScore < prevScore and guess == 'l':
+        elif curScore < prevScore and guess == 'L':
             self.score = self.score + 100
         else:
             self.score = self.score - 75
@@ -37,13 +37,13 @@ class Hilo:
         card = deal.newCard()
         while deal.getScore() > 0:
             print("The card is: ",card)
-            guess = input("Higher or lower? [h/l]").lower()
+            guess = input("Higher or lower? [H/L]").lower()
             card = deal.newCard()
             deal.compare(guess)
             print("Your card was ")
             print("Your score is: ",deal.getScore())
-            choice = input("Keep playing? [y/n]").lower()
-            if choice == 'n':
+            choice = input("Keep playing? [Y/N]").lower()
+            if choice == 'N':
                  print("Thanks for playing!")
             return
 
